ac3fbfc98322cffbd2c6e761e581cfe342c35c54
Teron <41672671+TeronKit@users.noreply.github.com>
2025-10-07 14:38:23 +0900

    Initial commit

---
+++ src/main/java/com/easeon/ss/quickmend/Easeon.java
@@ -0,0 +1,33 @@
+package com.easeon.ss.quickmend;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Easeon implements ModInitializer {
+    public static final String MOD_ID = "easeon-quickmend";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final ConfigManager CONFIG = new ConfigManager();
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("QuickMend Mod Initializing...");
+
+        CONFIG.load();
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            EaseonCommand.register(dispatcher);
+            LOGGER.info("Commands registered!");
+        });
+
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER) {
+            UseBlockCallback.EVENT.register(QuickMendHandler::onBlockUse);
+        }
+
+        LOGGER.info("QuickMend Mod Initialized!");
+    }
+}
